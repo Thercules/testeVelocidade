47574f0816433e3a2e65c07c26e656532fe2e70d
Thercules <thhercules2012@gmail.com>
2023-10-12 16:25:52 -0300

    second commit

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ $(document).ready(function () {
 				: $(".scroll-up-btn").removeClass("show");
 	}),
 		$(".scroll-up-btn").click(function () {
-			window.location.href =
-				"https://api.whatsapp.com/send?phone=5581987209676&text=Ol%C3%A1,%20eu%20gostaria%20de%20agendar%20uma%20consulta...";
+			window.open(
+				"https://api.whatsapp.com/send?phone=5581987209676&text=Ol%C3%A1,%20eu%20gostaria%20de%20agendar%20uma%20consulta...",
+				"_blank"
+			);
 		}),
 		$(".navbar .menu li a").click(function () {
 			$("html").css("scrollBehavior", "smooth");
@@ -17,9 +19,26 @@ $(document).ready(function () {
 		$(".menu-btn").click(function () {
 			$(".navbar .menu").toggleClass("active"),
 				$(".menu-btn i").toggleClass("active");
-		}),
+		});
+}),
+	$(document).ready(function () {
+		$(".carouselPics").owlCarousel({
+			items: 4,
+			margin: 0,
+			loop: !0,
+			dots: !0,
+			autoplay: !1,
+			autoplayTimeOut: 500,
+			autoplayHoverPause: !0,
+			responsive: {
+				0: { items: 3, nav: !1 },
+				768: { items: 4, nav: !1 },
+			},
+		});
+	}),
+	$(document).ready(function () {
 		$(".carousel").owlCarousel({
-			item: 4,
+			items: 4,
 			margin: 20,
 			loop: !1,
 			autoplay: !0,
@@ -30,21 +49,9 @@ $(document).ready(function () {
 				0: { items: 1, nav: !1 },
 				1e3: { items: 4, nav: !1 },
 			},
-		}),
-		$(document).ready(function () {
-			$(".carouselHome").owlCarousel({
-				items: 1,
-				margin: 0,
-				loop: !0,
-				dots: !1,
-				autoplay: !1,
-				autoplayTimeout: 2e3,
-				autoplayHoverPause: !1,
-				nav: !1,
-				navText: ["<button>&lt;</button>", "<button>&gt;</button>"],
-				responsive: { 0: { items: 1 } },
-			});
-		}),
+		});
+	}),
+	$(document).ready(function () {
 		$("#carousel-testemunials").owlCarousel({
 			navigation: !0,
 			slideSpeed: 300,
@@ -59,39 +66,9 @@ $(document).ready(function () {
 				0: { items: 1, nav: !1 },
 				768: { items: 2, nav: !1 },
 			},
-		}),
-		$(".carouselAbout").owlCarousel({
-			items: 1,
-			margin: 20,
-			loop: !0,
-			autoplay: !0,
-			autoplayTimeOut: 2e3,
-			autoplayHoverPause: !1,
-			responsive: { 0: { items: 1, nav: !1 } },
-		}),
-		$(".carouselAbout2").owlCarousel({
-			items: 3,
-			margin: 20,
-			dots: !1,
-			loop: !0,
-			autoplay: !0,
-			autoplayTimeOut: 2e3,
-			autoplayHoverPause: !0,
-			responsive: { 0: { items: 1, nav: !1 }, 0: { items: 2, nav: !1 } },
-		}),
-		$(".carouselPics").owlCarousel({
-			items: 4,
-			margin: 0,
-			loop: !0,
-			dots: !0,
-			autoplay: !1,
-			autoplayTimeOut: 500,
-			autoplayHoverPause: !0,
-			responsive: {
-				0: { items: 3, nav: !1 },
-				768: { items: 4, nav: !1 },
-			},
-		}),
+		});
+	}),
+	$(document).ready(function () {
 		$(".carouselPlanos").owlCarousel({
 			items: 5,
 			margin: 20,
@@ -104,32 +81,19 @@ $(document).ready(function () {
 				0: { items: 1, nav: !1 },
 				1e3: { items: 5, nav: !1 },
 			},
-		}),
-		$(".carouselTest").owlCarousel({
-			items: 2,
-			margin: 1,
-			loop: !0,
-			dots: !1,
-			autoplay: !1,
-			autoplayTimeOut: 2e3,
-			autoplayHoverPause: !1,
-			responsive: {
-				0: { items: 1, nav: !1 },
-				768: { items: 2, nav: !1 },
-			},
 		});
-});
+	});
 const modalZero = document.getElementById("modalZero"),
 	modalOne = document.getElementById("modalOne"),
 	openModalZeroBtn = document.getElementById("openModalZero"),
 	openModalOneBtn = document.getElementById("openModalOne"),
 	closeModalZeroBtn = document.querySelector(".closeModalZero"),
 	closeModalOneBtn = document.querySelector(".closeModalOne");
-function openModal(o) {
-	o.style.display = "block";
+function openModal(e) {
+	e.style.display = "block";
 }
-function closeModal(o) {
-	o.style.display = "none";
+function closeModal(e) {
+	e.style.display = "none";
 }
 openModalZeroBtn.addEventListener("click", function () {
 	openModal(modalZero);
@@ -143,10 +107,10 @@ openModalZeroBtn.addEventListener("click", function () {
 	closeModalOneBtn.addEventListener("click", function () {
 		closeModal(modalOne);
 	}),
-	window.addEventListener("click", function (o) {
-		o.target === modalZero && closeModal(modalZero),
-			o.target === modalOne && closeModal(modalOne);
+	window.addEventListener("click", function (e) {
+		e.target === modalZero && closeModal(modalZero),
+			e.target === modalOne && closeModal(modalOne);
 	}),
-	document.addEventListener("keydown", function (o) {
-		"Escape" === o.key && (closeModal(modalZero), closeModal(modalOne));
+	document.addEventListener("keydown", function (e) {
+		"Escape" === e.key && (closeModal(modalZero), closeModal(modalOne));
 	});
